c7cb9f8a549a3bf65f461aa122f70e6547d0e44a
Nisha Rohilla <rohilla.nisha15@gmail.com>
2025-04-05 16:29:36 +0530

    Changes : Clerk Authentication & Routing  Setup Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import { Button } from './components/ui/button'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import PublicLayout from "./Layouts/public-layout"
+import AuthenticationLayout from "./Layouts/auth-layout"
+
+import HomePage from "./routes/home"
+import SignInPage from "./routes/sign-in"
+import SignUpPage from "./routes/sign-up"
+
+import ProtectedRoutes from "./Layouts/protected-routes"
+import MainLayout from "./Layouts/main-layout"
 
 const App = () => {
   return (
-    <div className='text-blue-900'>
-      App
-    </div>
+    <Router>
+      <Routes>
+        {/* public routes */}
+        <Route element={<PublicLayout />}><Route index element={<HomePage />} />
+        </Route>
+
+        {/* authentication layout */}
+        <Route element={<AuthenticationLayout />}><Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
+        </Route>
+
+        {/* protected routes */}
+        <Route element={<ProtectedRoutes><MainLayout /></ProtectedRoutes>}>
+          {/* add all the protect routes */}
+        </Route>
+
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/Layouts/public-layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/ui/footer";
+import Header from "@/components/ui/header"
+import { Outlet } from "react-router-dom";
+
+
+function PublicLayout() {
+    return (
+        <div className="w-full">
+            {/* Handler to store the user data */}
+            <Header />
+            <Outlet />
+            <Footer />
+
+        </div>
+    )
+}
+
+export default PublicLayout
