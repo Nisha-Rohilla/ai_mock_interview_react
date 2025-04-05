0792db9186f9b39308604b2bc52c36e3ffdbdcdc
Nisha Rohilla <rohilla.nisha15@gmail.com>
2025-04-05 13:41:26 +0530

    Changes: Project initiated & setup completed with ShadCn UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='text-blue-900'>
+      App
+    </div>
+  )
+}
+
+export default App
+
